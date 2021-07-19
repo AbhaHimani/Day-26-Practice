2b9f1981f83e899ae620a10dabeae284f8bca49c
Abha Himani <74365827+AbhaHimani@users.noreply.github.com>
2021-07-19 19:27:18 +0530

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,13 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+total_words= sentence.split()
+for word in total_words:
+   lesgo=len(word)
+   print(f"{word}: {lesgo}")
+
+# Write your code below:
+
+
+
+
+
